5672faa1b4df13a9f1a42e85b1e9447c42b2be16
Brett Buscemi <Buscemi56@Bretts-MacBook-Air-2.local>
2017-11-24 10:37:12 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 // console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+var cards = ['queen, queen, king, king'];
+var cardsInPlay =[];
+
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped Queen");
+
+var cardTwo = cards[1];
+cardsInPlay.push('cardTwo');
+console.log("User flipped King");
+
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again.");
+	}
+
+}
